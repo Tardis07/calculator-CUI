03809a508c7a5ed531a50dfdfd7019616b9fe295
Tardis07 <944730698@qq.com>
2016-03-01 23:38:10 +0800

    Add selection page
    还不太清楚接下来是直接调用utils还是utils里写函数，只调用函数。。

---
+++ index.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+#encoding=utf8
+
+# File Name: main.py
+# Author: Tardis07
+# Creat Time: 2016-03-01 21:36
+
+from utils import *
+import math
+import os
+
+print 
+'''
+This is a Calculator in CUI mode. Please select the function to apply.
+1.一元二次方程(ax^2+bx+c=0)
+2.抛物线计算焦点坐标&准线方程
+3.平面内两点间距离
+4.空间内两点间距离
+'''
+selection=int(raw_input("Please input the function number:\n"))
+if selection == 1
+	
